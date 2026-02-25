cdef6b6a55efc635d7fe57959b8447e55115c5f3
shubhamwizd <shubhamwebdesigner5@gmail.com>
2026-02-25 11:45:33 +0530

    update

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyAHeRwgi7rPT2u-PueQ4Pz2mjhA5paWxPA",
     authDomain: "slowchat-ba812.firebaseapp.com",
     projectId: "slowchat-ba812",
-    storageBucket: "slowchat-ba812.firebasestorage.app",
+    storageBucket: "slowchat-ba812.appspot.com",
     messagingSenderId: "310630321728",
     appId: "1:310630321728:web:d03e74b00d1a50ceb419a5",
     measurementId: "G-JB5GRC7YLF"
